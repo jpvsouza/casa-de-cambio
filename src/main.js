a716636449ab2f317804c795529892758e1a1c9d
Joao Paulo Veloso <joaopaulox@outlook.com>
2023-03-02 20:08:39 -0300

    style: :lipstick: Light and dark mode implemented

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import '../style.css';
 // Importa a biblioteca sweet alert para mostrar erro mais bonito
 import Swal from 'sweetalert2';
 
-const input = document.querySelector('input');
+const input = document.getElementById('input');
 const btnPesquisar = document.querySelector('button');
 const grid = document.getElementById('card-holder');
 
@@ -22,10 +22,9 @@ input.addEventListener('change', () => {
 const getExchange = () => {
   clearGrid();
 
-  fetch(endpoint + moeda).then((response) =>
-    response
-      .json()
-      .then((data) => {
+  fetch(endpoint + moeda)
+    .then((response) =>
+      response.json().then((data) => {
         Object.keys(data.rates).forEach((cotacao) => {
           const card = document.createElement('div');
           card.setAttribute('class', 'h-100 col-sm-6 col-md-4 col-lg-3 card text-center');
@@ -33,15 +32,26 @@ const getExchange = () => {
           grid.appendChild(card);
         });
       })
-  ).catch((error) => {
-    Swal.fire({
-      title: 'Houve o seguinte erro:',
-      text: error.message,
-      icon: 'error',
-      confirmButtonText: 'Continue',
+    )
+    .catch((error) => {
+      Swal.fire({
+        title: 'Houve o seguinte erro:',
+        text: error.message,
+        icon: 'error',
+        confirmButtonText: 'Continue',
+      });
     });
-  });
 };
 
 // Adiciona o listener ao botão de pesquisar
 btnPesquisar.addEventListener('click', getExchange);
+
+// Função pra mudar de darkmode pra lightmode
+const mudarTema = () => {
+  const body = document.body;
+  body.dataset.bsTheme = body.dataset.bsTheme == 'light' ? 'dark' : 'light';
+};
+
+// Adicionando a função de mudar tema ao input
+const flexSwitch = document.getElementById('flexSwitch');
+flexSwitch.addEventListener('click', mudarTema);
